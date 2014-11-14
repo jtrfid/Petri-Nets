4529349c5b25603d406a5580ad8ffc08feec939d
Lea Vega <Lea Vega@LVR>
2014-11-14 15:55:23 -0600

    Interfaz

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,6 @@
+
+import java.util.HashSet;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,5 +11,24 @@
  * @author petergtam
  */
 public class Node {
+    private HashSet<Integer> marker;
+    private String type;
     
+    
+    public HashSet<Integer> getMarker() {
+        return marker;
+    }
+
+    public void setMarker(HashSet<Integer> marker) {
+        this.marker = marker;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
 }
--- src/PetriNetworks.java
+++ src/PetriNetworks.java
@@ -1,3 +1,9 @@
+
+import java.io.File;
+import java.util.HashSet;
+
+
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -8,5 +14,68 @@
  * @author Lea Vega
  */
 public class PetriNetworks {
+    int c[][]; //Incidence matrix 
+    int post[][];
+    int pre[][];
+    int m0[];
     
+    public void reachabilityGraph(){
+    
+      
+    }
+    
+    private HashSet<Integer> enableTransition(HashSet<Integer> m){
+        
+        return null;
+    }
+    
+    private HashSet<Integer> firingCondition(HashSet<Integer> m, HashSet<Integer> vk){
+    
+        return null;
+    }
+    
+    private boolean reversibility(){
+    
+      return true;
+    }
+    
+    private boolean boundedness(){
+    
+      return true;
+    }
+    
+    private boolean liveness(){
+    
+      return true;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] ar){
+     GraphViz gv = new GraphViz();
+      gv.addln(gv.start_graph());
+      gv.addln( "node [shape=plain];\n" +
+                " node [fillcolor=\"#EEEEEE\"];\n" +
+                " node [color=\"#EEEEEE\"];\n" +
+                " edge [color=\"#31CEF0\"];");
+      
+      
+      gv.addln("\"[09876]\" -> B [label=\"asddsfds\"];");
+      gv.addln(" t1 -> C;");
+      gv.addln(gv.end_graph());
+      
+       String type = "png";
+       File out = new File("out." + type);    // Windows
+       gv.writeGraphToFile( gv.getGraph(gv.getDotSource(), type ), out );
+    
+    }
+        
+   
 }
